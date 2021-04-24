5cc040373f7b971aa914aa57c76e1d411943eec9
Marko <59150246+MarkoNikolicViser@users.noreply.github.com>
2021-04-24 21:32:57 +0200

    Add files via upload
    hover bugs fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,14 @@ import lik from '../images/lik.svg'
 const Home=({dark})=>{
 
 const HomeRef=useRef(null);
+const Krug3Ref=useRef(null);
+
 
 useEffect(() => {
     if(dark===true){
     HomeRef.current.style.backgroundColor="#222831";
     HomeRef.current.style.color="#eeeeee";
-
+    Krug3Ref.current.style.backgroundColor="#eeeeee"
     }
     else{
         HomeRef.current.style.backgroundColor="#eeeeee";
@@ -25,7 +27,7 @@ useEffect(() => {
             <img className="lik" src={lik} alt=""/>
             <div className="x">
             <div className="krug"></div>
-            <div className="krug3"></div>
+            <div ref={Krug3Ref} className="krug3"></div>
         </div>
         <div className="x">
             <div className="krug2"></div>
